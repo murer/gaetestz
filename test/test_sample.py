1dba8f321f1da1675c10239a01ca4dba16f64a8a
murer <murer@dextra-sw.com>
2015-04-08 14:48:29 -0300

    x

--- test/test_sample.py
+++ test/test_sample.py
@@ -11,15 +11,10 @@ class SampleTestCase(unittest.TestCase):
 
 
   def setUp(self):
-    # First, create an instance of the Testbed class.
     self.testbed = testbed.Testbed()
-    # Then activate the testbed, which prepares the service stubs for use.
     self.testbed.activate()
-    # Create a consistency policy that will simulate the High Replication consistency model.
     self.policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=0)
-    # Initialize the datastore stub with this policy.
     self.testbed.init_datastore_v3_stub(consistency_policy=self.policy)
-    # Initialize memcache stub too, since ndb also uses memcache
     self.testbed.init_memcache_stub()
 
   def tearDown(self):
@@ -28,19 +23,10 @@ class SampleTestCase(unittest.TestCase):
   def testEventuallyConsistentGlobalQueryResult(self):
     class TestModel(ndb.Model):
       pass
-
-    import src.main as main
-    print main.Ping
-
     user_key = ndb.Key('User', 'ryan')
-    # Put two entities
     ndb.put_multi([TestModel(parent=user_key), TestModel(parent=user_key)])
-
-    # Global query doesn't see the data.
     self.assertEqual(0, TestModel.query().count(3))
-    # Ancestor query does see the data.
     self.assertEqual(2, TestModel.query(ancestor=user_key).count(3))
 
 if __name__ == '__main__':
     unittest.main()
-    #unittest.TextTestRunner(verbosity=2).run(unittest.loader.TestLoader().discover('.'))
