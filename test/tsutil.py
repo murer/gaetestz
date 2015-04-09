2cdf0ce450056ee8001c4e1d9f01804cff3b26f4
murer <murer@dextra-sw.com>
2015-04-09 08:16:22 -0300

    x

--- test/tsutil.py
+++ test/tsutil.py
@@ -6,21 +6,21 @@ import os
 def fix():
     sdk_path = '.gen/google_appengine'
     if os.path.exists(os.path.join(sdk_path, 'platform/google_appengine')):
-      sys.path.insert(0, os.path.join(sdk_path, 'platform/google_appengine'))
+        sys.path.insert(0, os.path.join(sdk_path, 'platform/google_appengine'))
     else:
-      sys.path.insert(0, sdk_path)
+        sys.path.insert(0, sdk_path)
     sys.path.insert(0, '.')
     import dev_appserver
     dev_appserver.fix_sys_path()
     try:
-      import appengine_config
+        import appengine_config
     except ImportError:
-      print "Note: unable to import appengine_config."
+        print "Note: unable to import appengine_config."
 
 def main(test_path):
     suite = unittest.loader.TestLoader().discover(test_path)
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
-    main('test')
-    
+        main('test')
+        
