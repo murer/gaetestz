fe08a5a6a0a639ecc605515ff408aff7b9815e2e
murer <murer@dextra-sw.com>
2016-02-17 17:30:05 -0200

    docker

--- test/test_case.py
+++ test/test_case.py
@@ -7,7 +7,10 @@ gae = GaeTestServer()
 class TestCase(unittest.TestCase):
 
     def setUp(self):
+        import main
         gae.boot_gae()
+        gae.boot_web(8080, main.app)
+        gae.server_forever_background()
 
     def tearDown(self):
         gae.shutdown()
--- test/test_one.py
+++ test/test_one.py
@@ -1,5 +1,6 @@
 
 import test_case
+import httplib
 from google.appengine.ext import ndb
 
 class OneTestCase(test_case.TestCase):
@@ -11,3 +12,14 @@ class OneTestCase(test_case.TestCase):
         ndb.put_multi([TestModel(parent=user_key), TestModel(parent=user_key)])
         self.assertEqual(0, TestModel.query().count(3))
         self.assertEqual(2, TestModel.query(ancestor=user_key).count(3))
+
+    def test_twice(self):
+        self.test_one()
+
+    def test_web(self):
+        conn = httplib.HTTPConnection('localhost', test_case.gae.port)
+        conn.request('GET', '/s/ping')
+        resp = conn.getresponse()
+        print resp.status, resp.reason
+        print resp.read()
+        conn.close()
